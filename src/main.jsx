7253453487b79465f61558c717a79669f961797a
usnho3010-debug <usnho3010@gmail.com>
2026-06-08 00:00:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-﻿import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageProvider.jsx'
 
+const routerBasename = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/mailrandom'>
+    <BrowserRouter basename={routerBasename}>
       <LanguageProvider>
         <App />
       </LanguageProvider>
